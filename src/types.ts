c70aad838ba7cb85620adfa8fb44e7707455cec3
Abderazak Amiar <amiar.abderazak@gmail.com>
2025-10-19 09:16:40 +0100

    chore: update types file

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,11 @@ export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 export type AuthLayoutProps = {
   children: React.ReactNode;
 };
+
+export type AuthState = {
+  username: string;
+  role: string | null;
+  isAuthenticated: boolean;
+  login: (username: string, role: string) => boolean;
+  logout: () => void;
+};
